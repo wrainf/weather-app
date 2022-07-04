93d05394e1b9183f670c34e293171d6a18a1f1ce
wrainf <amirfaiz1806@gmail.com>
2022-07-04 22:16:53 +0800

    completed loadMainWeatherContainer function

---
+++ src/loadMainWeatherContainer.js
@@ -0,0 +1,29 @@
+function loadMainWeatherContainer() {
+  const container = document.createElement('div');
+  const temp = document.createElement('div');
+  const locationDetails = document.createElement('div');
+  const city = document.createElement('div');
+  const dayDetails = document.createElement('div');
+  const time = document.createElement('div');
+  const date = document.createElement('div');
+  const weather = document.createElement('div');
+
+  dayDetails.appendChild(time);
+  dayDetails.appendChild(date);
+  locationDetails.appendChild(city);
+  locationDetails.appendChild(dayDetails);
+
+  temp.setAttribute('id', 'temperature');
+  city.setAttribute('id', 'city');
+  time.setAttribute('id', 'time');
+  date.setAttribute('id', 'date');
+  weather.setAttribute('id', 'weather');
+
+  container.appendChild(temp);
+  container.appendChild(locationDetails);
+  container.appendChild(weather);
+  container.setAttribute('id', 'main-weather');
+  return container;
+}
+
+export default loadMainWeatherContainer;
