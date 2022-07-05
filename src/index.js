1a46e82ca30eff8b36545b174fbdad7c41a96ea7
wrainf <amirfaiz1806@gmail.com>
2022-07-05 11:31:48 +0800

    completed populate sidebar function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import loadPage from './loadPage';
 import './style.css';
 import loadMainWeatherContainer from './loadMainWeatherContainer';
+import boxDeatils from './boxDetails';
 
 document.body.appendChild(loadPage());
 const main = document.querySelector('#main');
+const sidebar = document.querySelector('#sidebar');
 main.appendChild(loadMainWeatherContainer());
 
 const temperatureDiv = document.querySelector('#temperature');
 const cityDiv = document.querySelector('#city');
 const hourDiv = document.querySelector('#hour');
 const minuteDiv = document.querySelector('#minute');
-
 const dayDiv = document.querySelector('#day');
 const dateDiv = document.querySelector('#date');
 const monthDiv = document.querySelector('#month');
+
 const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Octr', 'Nov', 'Dec'];
+const weather = document.querySelector('#weather');
 
 async function getWeather() {
   const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=New York&units=metric&APPID=2a3e6417c89470aaa7587fa599ac7255');
@@ -42,12 +45,23 @@ function setTime(date) {
   monthDiv.textContent = months[date.getMonth()];
 }
 
+function populateSidebar(weatherData) {
+  sidebar.appendChild(boxDeatils('Feels Like', `${weatherData.main.feels_like}°`));
+  sidebar.appendChild(boxDeatils('High', `${weatherData.main.temp_max}°`));
+  sidebar.appendChild(boxDeatils('Low', `${weatherData.main.temp_min}°`));
+  sidebar.appendChild(boxDeatils('Humidity', `${weatherData.main.humidity}%`));
+  sidebar.appendChild(boxDeatils('Pressure', `${weatherData.main.humidity}hPa`));
+  sidebar.appendChild(boxDeatils('Wind', `${weatherData.wind.speed}m/s`));
+}
+
 async function setData() {
   const weatherData = await getWeather();
-  temperatureDiv.textContent = weatherData.main.temp;
+  temperatureDiv.textContent = `${weatherData.main.temp}°`;
   cityDiv.textContent = weatherData.name;
   const date = getDate(weatherData.timezone);
   setTime(date);
+  weather.textContent = weatherData.weather[0].main;
+  populateSidebar(weatherData);
 }
 
 setData();
