5c0d29c3018c8d11eeb8528599a63faf39b47a42
wrainf <amirfaiz1806@gmail.com>
2022-07-05 10:46:50 +0800

    added more date details

--- src/loadMainWeatherContainer.js
+++ src/loadMainWeatherContainer.js
@@ -5,22 +5,40 @@ function loadMainWeatherContainer() {
   const city = document.createElement('div');
   const dayDetails = document.createElement('div');
   const time = document.createElement('div');
-  const date = document.createElement('div');
+  const fullDate = document.createElement('div');
   const weather = document.createElement('div');
 
   dayDetails.appendChild(time);
-  dayDetails.appendChild(date);
+  dayDetails.appendChild(fullDate);
   locationDetails.appendChild(city);
   locationDetails.appendChild(dayDetails);
 
   temp.setAttribute('id', 'temperature');
   city.setAttribute('id', 'city');
   time.setAttribute('id', 'time');
-  date.setAttribute('id', 'date');
+  fullDate.setAttribute('id', 'fullDate');
   weather.setAttribute('id', 'weather');
 
   locationDetails.setAttribute('id', 'location-details');
   dayDetails.setAttribute('id', 'day-details');
+
+  const hour = document.createElement('div');
+  const minute = document.createElement('div');
+  hour.setAttribute('id', 'hour');
+  minute.setAttribute('id', 'minute');
+  time.appendChild(hour);
+  time.appendChild(minute);
+
+  const day = document.createElement('div');
+  const date = document.createElement('div');
+  const month = document.createElement('div');
+  day.setAttribute('id', 'day');
+  date.setAttribute('id', 'date');
+  month.setAttribute('id', 'month');
+
+  fullDate.appendChild(day);
+  fullDate.appendChild(date);
+  fullDate.appendChild(month);
 
   container.appendChild(temp);
   container.appendChild(locationDetails);
