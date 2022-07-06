83ebb2f0311601965d008e9fe6a32972fb929e1f
wrainf <amirfaiz1806@gmail.com>
2022-07-06 13:04:30 +0800

    set id for values

--- src/boxDetails.js
+++ src/boxDetails.js
@@ -1,4 +1,4 @@
-function boxDeatils(title, value) {
+function boxDetails(title, value) {
   const container = document.createElement('div');
   const key = document.createElement('div');
   const val = document.createElement('div');
@@ -9,7 +9,8 @@ function boxDeatils(title, value) {
   container.appendChild(key);
   container.appendChild(val);
   container.setAttribute('id', 'box-information');
+  val.setAttribute('id', 'value');
   return container;
 }
 
-export default boxDeatils;
+export default boxDetails;
